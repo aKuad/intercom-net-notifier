b685801afe6eb36ddeb3846b25c99334e3d72cd2
aKuad <53811809+aKuad@users.noreply.github.com>
2026-08-08 16:16:12 +0900

    Just only connect Wi-Fi
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

---
+++ include/comm_secrets_template.h
@@ -0,0 +1,15 @@
+/**
+ * @file comm_secrets_template.h
+ *
+ * Secret constants definition (e.g. token, password, etc.)
+ *
+ * Copy it to `comm_secrets.h` and fill out values
+ */
+#ifndef _COMM_SECRETS_H_
+#define _COMM_SECRETS_H_
+
+const char *WIFI_SSID = "";
+const char *WIFI_PASS = "";
+
+
+#endif /* _COMM_SECRETS_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
+/**
+ * @file main.cpp
+ *
+ * Home intercom notifier via Internet messenger DIY with ESP32
+ */
+
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "comm_secrets.h"
 
+
+/* File internal constants */
+static const uint8_t ONBOARD_LED = 2; // At ESP32 Dev Module
+
+
+/* Setup section - runs once */
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ONBOARD_LED, OUTPUT);
+  digitalWrite(ONBOARD_LED, LOW);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  if(WiFi.status() == WL_NO_SHIELD) {
+    while(1) {
+      digitalWrite(ONBOARD_LED, HIGH);
+      delay(500);
+      digitalWrite(ONBOARD_LED, LOW);
+      delay(500);
+    }
+  }
 }
 
+
+/* Loop section - runs repeatedly */
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if(WiFi.isConnected())
+    digitalWrite(ONBOARD_LED, HIGH);
+  else
+    digitalWrite(ONBOARD_LED, LOW);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(500); // Prevent unnecessary too fast loop
 }
