dcd16e9b7f1aa3384ce16014da51d699c7e8de5a
aKuad <53811809+aKuad@users.noreply.github.com>
2026-08-19 22:57:00 +0900

    Message posting on main process implement with test key input
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,27 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
+#include "messenger_post_simple.h"
 #include "comm_secrets.h"
 
 
 /* File internal constants */
-static const uint8_t ONBOARD_LED = 2; // At ESP32 Dev Module
+static const uint8_t ONBOARD_LED = 2;   // At ESP32 Dev Module
+static const uint8_t TEST_KEY    = 15;
 
 
 /* Setup section - runs once */
 void setup() {
   pinMode(ONBOARD_LED, OUTPUT);
   digitalWrite(ONBOARD_LED, LOW);
+  pinMode(TEST_KEY, INPUT);
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
+  // When no Wi-Fi module detected, blink indication and do nothing more
   if(WiFi.status() == WL_NO_SHIELD) {
     while(1) {
-      digitalWrite(ONBOARD_LED, HIGH);
-      delay(500);
-      digitalWrite(ONBOARD_LED, LOW);
-      delay(500);
+      digitalWrite(ONBOARD_LED, HIGH); delay(500);
+      digitalWrite(ONBOARD_LED, LOW);  delay(500);
     }
   }
 }
@@ -33,10 +35,23 @@ void setup() {
 
 /* Loop section - runs repeatedly */
 void loop() {
+  // Wi-Fi connected indication
   if(WiFi.isConnected())
     digitalWrite(ONBOARD_LED, HIGH);
   else
     digitalWrite(ONBOARD_LED, LOW);
 
-  delay(500); // Prevent unnecessary too fast loop
+  // On signal detected process
+  if(WiFi.isConnected() && digitalRead(TEST_KEY)) {
+    discord_webhook_post(DISCORD_WEBHOOK_URL, "Doorbell rang");
+    line_broadcast_post(LINE_CHANNEL_ACCESS_TOKEN, "Doorbell rang");
+
+    // Quick blink for signal detected indication
+    digitalWrite(ONBOARD_LED,  LOW); delay(100);
+    digitalWrite(ONBOARD_LED, HIGH); delay(100);
+    digitalWrite(ONBOARD_LED,  LOW); delay(100);
+    digitalWrite(ONBOARD_LED, HIGH); delay(100);
+  }
+
+  delay(250); // Prevent unnecessary too fast loop
 }
