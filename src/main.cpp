761359e3a5bebfd1f25bfc5ed16d55f38d13377f
aKuad <53811809+aKuad@users.noreply.github.com>
2026-08-23 21:55:51 +0900

    Test key enable/disable switcing macro for no test-key usage
    Signed-off-by: aKuad <53811809+aKuad@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,25 @@
 #include "messenger_post_simple.h"
 #include "comm_secrets.h"
 
+#define TEST_KEY_ENABLE
+
 
 /* File internal constants */
 static const uint32_t SIGNAL_INTERVAL_MS = 10000;
 static const uint8_t ONBOARD_LED  = 2;  // At ESP32 Dev Module
 static const uint8_t INTERCOM_SIG = 4;
-static const uint8_t TEST_KEY     = 15;
-
+#ifdef TEST_KEY_ENABLE
+  static const uint8_t TEST_KEY     = 15;
+#endif
 
 /* Setup section - runs once */
 void setup() {
   pinMode(ONBOARD_LED, OUTPUT);
   digitalWrite(ONBOARD_LED, LOW);
-  pinMode(TEST_KEY, INPUT);
   pinMode(INTERCOM_SIG, INPUT);
+  #ifdef TEST_KEY_ENABLE
+    pinMode(TEST_KEY, INPUT);
+  #endif
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   // When no Wi-Fi module detected, blink indication and do nothing more
@@ -38,14 +43,21 @@ void setup() {
 
 /* Loop section - runs repeatedly */
 void loop() {
-  // Wi-Fi connected indication
+  // Wi-Fi connected LED indication
   if(WiFi.isConnected())
     digitalWrite(ONBOARD_LED, HIGH);
   else
     digitalWrite(ONBOARD_LED, LOW);
 
+  // Signal detected process dispatch condition checking
+  #ifdef TEST_KEY_ENABLE
+    bool signal_detected = WiFi.isConnected() && (digitalRead(INTERCOM_SIG) || digitalRead(TEST_KEY));
+  #else
+    bool signal_detected = WiFi.isConnected() && digitalRead(INTERCOM_SIG);
+  #endif
+
   // On signal detected process
-  if(WiFi.isConnected() && (digitalRead(INTERCOM_SIG) || digitalRead(TEST_KEY))) {
+  if(signal_detected) {
     discord_webhook_post(DISCORD_WEBHOOK_URL, "Doorbell rang");
     line_broadcast_post(LINE_CHANNEL_ACCESS_TOKEN, "Doorbell rang");
 
